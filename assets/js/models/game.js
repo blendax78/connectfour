19667b3a545487ff1913716ceef4e54a1533484e
blendax78 <eric@vintimilla.org>
2017-04-05 21:31:55 -0400

    updates to game model & index view

--- assets/js/models/game.js
+++ assets/js/models/game.js
@@ -3,11 +3,16 @@ Salesloft.Models.Piece = Backbone.Model.extend({
     {
       // Javascript Model Initialization for a Game.
       // If we have to do any kind of changes to Game properties, this is where to do it.
+      // var pieces = new Salesloft.Collections.Players(this.get('players'))
     },
 
     url: function()
     {
       // Server endpoint
         return '/game';
+    },
+
+    parse: function(response) {
+      console.log('hi', response);
     }
 });
